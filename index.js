607778a3e475a385336cbd3bbf9bb9c356846d02
kakon4366 <kakonbarman4366@gmail.com>
2022-05-23 16:01:08 +0600

    mongodb connect and all tools get success

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vtyhe.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+	try {
+		await client.connect();
+		const toolCollection = client
+			.db("tools-manufacturer")
+			.collection("tools");
+
+		app.get("/tools", async (req, res) => {
+			const query = {};
+			const result = await toolCollection.find(query).toArray();
+			res.send(result);
+		});
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
+
+//root api
 app.get("/", (req, res) => {
 	res.send("Tools Menufacturer server is running!");
 });
