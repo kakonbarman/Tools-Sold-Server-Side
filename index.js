56982205fa2aeb9cd151382e4e4d333e234a9d92
kakon4366 <kakonbarman4366@gmail.com>
2022-05-24 12:23:04 +0600

    database user add success

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vtyhe.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://tools_manufacturer_admin:RZ61uB1zmtZgxNVJ@cluster0.vtyhe.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -21,17 +22,45 @@ async function run() {
 		await client.connect();
 		const toolCollection = client
 			.db("tools-manufacturer")
-			.collection("tools");
+			.collection("products");
+
+		const usersCollection = client
+			.db("tools-manufacturer")
+			.collection("users");
+
+		//user login with jwt
+		app.put("/user/:email", async (req, res) => {
+			const email = req.params.email;
+			const user = req.body;
+			console.log(user);
+			const filter = { email: email };
+			const options = { upsert: true };
+			const updateDoc = {
+				$set: user,
+			};
+			const result = await usersCollection.updateOne(
+				filter,
+				updateDoc,
+				options
+			);
+			const token = jwt.sign(
+				{ email: email },
+				process.env.ACCESS_TOKEN_SECRET,
+				{ expiresIn: "6h" }
+			);
+			res.send({ result, token });
+		});
 
 		//get all tools
-		app.get("/tools", async (req, res) => {
+		app.get("/products", async (req, res) => {
+			console.log("sob thik ache");
 			const query = {};
 			const result = await toolCollection.find(query).toArray();
 			res.send(result);
 		});
 
 		//get single tools
-		app.get("/tool/:id", async (req, res) => {
+		app.get("/product/:id", async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: ObjectId(id) };
 			const result = await toolCollection.findOne(query);
