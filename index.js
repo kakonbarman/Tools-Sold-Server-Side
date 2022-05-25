aea04fe3cdb3f424f27c6e09fe0334ea7529dfa1
kakon4366 <kakonbarman4366@gmail.com>
2022-05-25 11:50:57 +0600

    admin check api working success

--- index.js
+++ index.js
@@ -149,6 +149,14 @@ async function run() {
 			res.send(result);
 		});
 
+		// admin
+		app.get("/admin/:email", async (req, res) => {
+			const email = req.params.email;
+			const user = await usersCollection.findOne({ email: email });
+			const isAdmin = user.role === "admin";
+			res.send({ admin: isAdmin });
+		});
+
 		//post orderInfo to database
 		app.post("/order", verifyJWT, async (req, res) => {
 			const orderInfo = req.body;
